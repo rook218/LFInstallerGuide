a930547d42208f2a31208a97358e8d8d1bcebc63
Brian Rook <rook218@gmail.com>
2020-08-13 17:18:49 -0400

    Added the primary forms option conditionally

--- guide.js
+++ guide.js
@@ -79,7 +79,10 @@ class ServerList {
 }
 // function to add a new server object on clicking the button
 const addServerButton = document.querySelector('button.add-server');
-addServerButton.addEventListener('click', function() {
+let formsOptions = document.querySelectorAll('input[name=forms]');
+addServerButton.addEventListener('click', addNewServer);
+
+function addNewServer() {
     // create a new server node
     let element = document.createElement('div');
     element.innerHTML = serverHTML;
@@ -90,37 +93,51 @@ addServerButton.addEventListener('click', function() {
     serverContainer.appendChild(element);
 
     // add the event listener so it can delete itself
-    element.querySelector('.delete-container').addEventListener('click', deleteSelf);
-});
+    element.querySelector('.delete-container').addEventListener('click', function() {
+        this.closest('.server').remove();
+    });
 
-// function to delete 
-function deleteSelf() {
-    this.closest('.server').remove();
+    // updates the forms options variable to include the new
+    // server and adds an event listener
+    formsOptions = document.querySelectorAll('input[name=forms]');
+    formsOptions.forEach(formsOption => {
+        formsOption.addEventListener('change', onFormsServerChange);
+    });
 }
 
 // on selecting the form install button on anything, checks if
 // there will be multiple forms servers. if yes, adds the primary
 // forms option to all of them. If no, removes them
-const formsOptions = document.querySelectorAll('input[name=forms]');
-formsOptions.addEventListener('change', function() {
-    let numberFormsServers = 0;
-    let checkedFormsOptions = document.querySelectorAll('input[name=forms]:checked');
+function onFormsServerChange() {
+    // removes the option globally to start
+    document.querySelectorAll('.primary-forms').forEach(primaryOption => {
+            primaryOption.remove();
+        })
+        // creates the element that we'll append
     const formsPrimaryHTML =
         `<h5>Primary Forms Server?</h5>
-			<input type="radio" name="public" value="yes">
-			<label for="yes">Yes</label>
-			<input type="radio" name="public" value="no">
-			<label for="no">No</label>`;
+	   <input type="radio" name="public" value="yes">
+	   <label for="yes">Yes</label>
+	   <input type="radio" name="public" value="no">
+	   <label for="no">No</label>`;
+    const formsOptionsElement = document.createElement('div');
+    formsOptionsElement.classList.add('option')
+    formsOptionsElement.classList.add('primary-forms');
+    formsOptionsElement.innerHTML = formsPrimaryHTML;
+
+    // if we have multiple forms servers, append the option to every one that's checked
+    let checkedFormsOptions = document.querySelectorAll('input[name=forms]:checked');
     if (checkedFormsOptions.length > 1) {
-        const formsOptionsElement = document.createElement('div');
-        formsOptionsElement.classList.add('option');
-        formsOptionsElement.innerHTML = formsPrimaryHTML;
         checkedFormsOptions.forEach(function(checkbox) {
-            checkbox.closest('.server').find('.config-options')
-                .appendChild(formsOptionsElement)
+            let localOption = formsOptionsElement.cloneNode();
+            localOption.innerHTML = formsPrimaryHTML;
+            checkbox.closest('.server').querySelector('.config-options')
+                .appendChild(localOption)
         });
     }
-})
+
+}
+
 
 
 // on clicking the return button, returns a ServerList object
