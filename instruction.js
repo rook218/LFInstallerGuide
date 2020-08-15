be87731ab5b4b421bf20445b9ac884992a9f313e
Brian Rook <rook218@gmail.com>
2020-08-15 10:02:47 -0400

    Added basic instructions for LFS, LFDS, and Forms

--- instruction.js
+++ instruction.js
@@ -1,18 +1,23 @@
 // defines servers and serverlist classes
 class Server {
-    constructor(name, forms, webAccess, webLink, workflow, lfs, lfds, sql, primaryForms) {
+    constructor(name, forms, webAccess, webLink, workflow, lfs, lfds, sql, isPublic, domainJoined, primaryForms) {
         this.portsToLFDS = [];
         this.portsToLFS = [];
 
         this.name = name;
-        this.forms = forms;
         this.webAccess = webAccess;
         this.webLink = webLink;
         this.workflow = workflow;
         this.lfs = lfs;
         this.lfds = lfds;
         this.sql = sql;
+
+        this.isPublic = isPublic;
+        this.domainJoined = domainJoined;
+
+        this.forms = forms;
         this.primaryForms = primaryForms;
+        this.changeFormsEndpoints = false;
 
         if (!this.lfds) {
             this.portsToLFDS.push(443, 5048, 5049);
@@ -67,7 +72,11 @@ class Server {
         this.portsToLFS = [...new Set(this.portsToLFS)];
         this.portsToLFS.sort(function(first, second) {
             return first > second;
-        })
+        });
+
+        if (this.forms && !this.primaryForms) {
+            this.changeFormsEndpoints = true;
+        }
     }
 }
 
@@ -78,6 +87,15 @@ class ServerList {
     }
 }
 
+class Instruction {
+    constructor(step, responsibility, required, header, substeps) {
+        this.step = step;
+        this.responsibility = responsibility;
+        this.required = required;
+        this.header = header;
+        this.substeps = substeps;
+    }
+}
 
 // on clicking the return button, returns a ServerList object
 // which contains a list of all servers as configured and their properties
@@ -96,15 +114,87 @@ function createServerList() {
         const lfds = thisServer.querySelector('input[name=lfds]').checked;
         const sql = thisServer.querySelector('input[name=sql]').checked;
         const domainJoined = thisServer.querySelector('input[name=dmz]').value === 'Yes';
-        const public = thisServer.querySelector('input[name=public]').value === 'Yes';
+        const isPublic = thisServer.querySelector('input[name=public]').value === 'Yes';
 
         const primaryFormsOption = thisServer.querySelector('input[name=formsPrimary]');
         const primaryForms = primaryFormsOption == null ? false : primaryFormsOption.value === 'Yes';
 
         let server = new Server(name, forms, webAccess, webLink, workflow,
-            lfs, lfds, sql, domainJoined, public, primaryForms);
+            lfs, lfds, sql, isPublic, domainJoined, primaryForms);
         serverList.servers.push(server);
     });
     console.log(serverList.servers);
     return serverList;
+}
+
+function generateInstructions(serverList) {
+    let instructions = [];
+
+    //#region establishes the overall configuration
+    // TODO: create an EnvironmentConfiguration object if this gets unweildy 
+    // or change the ServerList object to be the config object and run this in the constructor
+    let lfdsServerName;
+    let lfsServerName;
+    let sqlServerName;
+
+    let hasPublicWebLink = false;
+
+    let secondaryFormsServerNames = [];
+    let primaryFormsServerName;
+
+    serverList.forEach(server => {
+        if (server.lfds) {
+            lfdsServerName = server.name;
+        }
+        if (server.lfs) {
+            lfsServerName = server.name;
+        }
+        if (server.sql) {
+            sqlServerName = server.name;
+        }
+        if (server.primaryForms) {
+            primaryFormsServerName = server.name;
+        }
+        if (server.forms) {
+            secondaryFormsServerNames.push(server.name);
+        }
+        if (server.webLink && server.isPublic) {
+            hasPublicWebLink = true;
+        }
+    });
+    //#endregion
+
+    // generates an array of instructions from new Instructions objects, which we can clean up later
+    serverList.forEach(server => {
+        if (server.lfds) {
+            instructions.push(new Instruction(110, "VAR", true, "Install LFDS on " + lfdsServerName + ".", null));
+            instructions.push(new Instruction(120, "VAR", true, "Open LFDS and install master license on " + lfdsServerName + ".", null));
+        }
+        if (server.lfs) {
+            instructions.push(new Instruction(200, "VAR", true, "Install LFS on " + lfsServerName + ".", [
+                "Use the database SA account set up in the pre-config steps.",
+                "If using LFDS, use " + lfdsServerName + " as the licensing server."
+            ]));
+            instructions.push(new Instruction(210, "VAR", true, "Install desktop client with admin console and connect to your new instance.", [
+                "If this is a migration: back up the SQL DBs from the old SQL server to the new, copy over all volumes, and run the Register Repository task in the admin console",
+                "If this is a new install: Create a new repository from the admin console."
+            ]));
+        }
+        if (server.primaryForms) {
+            instructions.push(new Instruction(300, "VAR", true, "Install Forms on " + primaryFormsServerName + ".", ));
+            instructions.push(new Instruction(310, "VAR", true, "Navigate to localhost/FormsConfig on " + server.name + " and configure with the following options:", [
+                "Database page: DB name should be " + sqlServerName + "\\SQLEXPRESS by default and the user account is the SA account established already.",
+                "Server page: Server name should be " + primaryFormsServerName + "/Forms by default."
+            ]));
+        }
+
+        // TODO: add STS options and instructions
+        if (server.forms && !server.primaryForms) {
+            instructions.push(new Instruction(350, "VAR", true, "Install Forms on " + server.name + "."))
+            instructions.push(new Instruction(310, "VAR", true, "Navigate to localhost/FormsConfig on " + server.name + " and configure with the following options:", [
+                "Database page: DB name should be " + sqlServerName + "\\SQLEXPRESS by default and the user account is the SA account established already.",
+                "Server page: Server name should be " + primaryFormsServerName + "/Forms by default."
+            ]));
+        }
+    })
 }
